44f8b43360bf9731c7a87f821a8da55b3e9cfe1f
GitHub Action <action@github.com>
2025-11-27 19:31:52 +0000

    Bump version to 0.1.5

--- webquiz_stress_test/__init__.py
+++ webquiz_stress_test/__init__.py
@@ -1,5 +1,5 @@
 """WebQuiz Stress Test - Performance testing tool for WebQuiz servers"""
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __author__ = "Oleksandr Liabakh"
 __email__ = "oduvan@gmail.com"
